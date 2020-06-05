8b7785aef45239d73543bfe58b3619c5b109bf9e
SamuelSebastianMartin <sm2@soas.ac.uk>
2020-06-05 16:30:03 +0100

    added data class to Helpers.py

--- Helpers.py
+++ Helpers.py
@@ -1,8 +1,40 @@
 #! /usr/bin/env python3
 
 """
-This is to provide data which can be used with various 
-neural network programs
+    This is to provide data which can be used with various 
+    neural network programs.
 """
 
 import numpy as np
+
+
+class Data:
+    def __init__(self, size):
+        self.size = size
+        self.gradient = np.random.randint(-10, 11)
+        self.intercept = np.random.randint(-5, 6)
+        self.x = np.random.rand(self.size) * 10
+
+    def error_added(self):
+        """
+        Alternative x values with a bit of wobble
+        """
+        self.y = self.intercept + self.gradient * self.x
+        error = np.random.rand(self.size) / 10
+        return self.x
+
+    def linear_regres(self):
+        """
+        Returns linear function: y = m*x + c
+        """
+        self.y = self.intercept + self.gradient * self.x
+        return self.x, self.y
+
+    def linear_categ(self):
+        """
+        Returns 0 or 1, separated at y = m*x + c
+        i.e. y - m*x > c
+        """
+        y_temp = np.random.rand(self.size) * 10
+        discrim = y_temp - (self.gradient * self.x)  #  y - m*x
+        self.y = np.where(y_temp > self.intercept, 1, 0)
